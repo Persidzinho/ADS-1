89cdc6dfe6a77cd756c861186e576658b2f4c69c
Никита Чванов <143615653+Persidzinho@users.noreply.github.com>
2026-03-01 17:12:19 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,29 +1,69 @@
 // Copyright 2022 NNTU-CS
 #include <cstdint>
+#include <cmath>
 #include "alg.h"
 
-
 bool checkPrime(uint64_t value) {
-  // вставьте код функции
-  return true;
+    if (value < 2) return false;
+    if (value == 2) return true;
+    if (value % 2 == 0) return false;
+    
+    for (uint64_t i = 3; i * i <= value; i += 2) {
+        if (value % i == 0) {
+            return false;
+        }
+    }
+    return true;
 }
 
 uint64_t nPrime(uint64_t n) {
-  // вставьте код функции
-  return 2;
+    if (n == 0) return 0;
+    if (n == 1) return 2;
+    
+    uint64_t count = 1;
+    uint64_t num = 3;
+    
+    while (count < n) {
+        if (checkPrime(num)) {
+            count++;
+            if (count == n) {
+                return num;
+            }
+        }
+        num += 2;
+    }
+    return 2;
 }
 
 uint64_t nextPrime(uint64_t value) {
-  // вставьте код функции
-  return 2;
+    uint64_t num = value + 1;
+    if (num < 2) num = 2;
+    
+    while (true) {
+        if (checkPrime(num)) {
+            return num;
+        }
+        num++;
+    }
 }
 
 uint64_t sumPrime(uint64_t hbound) {
-  // вставьте код функции
-  return 2;
+    uint64_t sum = 0;
+    for (uint64_t i = 2; i < hbound; i++) {
+        if (checkPrime(i)) {
+            sum += i;
+        }
+    }
+    return sum;
 }
 
 uint64_t twinPrimes(uint64_t lbound, uint64_t hbound) {
-  // вставьте код функции
-  return 1;
+    uint64_t count = 0;
+    
+    for (uint64_t i = lbound; i + 2 < hbound; i++) {
+        if (checkPrime(i) && checkPrime(i + 2)) {
+            count++;
+        }
+    }
+    return count;
 }
